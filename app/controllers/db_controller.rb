80cffb47e062d05e0c698bb6806c578dab10a063
Santhosh <santhoshonet@gmail.com>
2011-08-05 01:20:46 -0700

    running rake tasks in controlle

--- app/controllers/db_controller.rb
+++ app/controllers/db_controller.rb
@@ -2,10 +2,6 @@ class DbController < ApplicationController
 
   def reset
 
-    Site.destroy_all
-    SiteContent.destroy_all
-    SqlInjectionQuery.destroy_all
-
     system "rake db:reset &"
 
     render :text =>  "done"
